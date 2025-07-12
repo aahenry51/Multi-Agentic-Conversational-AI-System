46fc7e39d3af405c69d5be2f5b52c5fdf42a757e
Alphi <aahenry51@gmail.com>
2025-07-12 03:50:49 -0400

    React Axios Setup

--- convAI/app/routes/home.tsx
+++ convAI/app/routes/home.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import './styles.css'; 
+import { postSignUpData } from '~/api/request';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +10,84 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+
+
 export default function Home() {
-  return <Welcome />;
+
+  const handleSubmit = async (e:any) => {
+        e.preventDefault();
+        //console.log(inputFirst)
+        postSignUpData({
+          first: inputFirst,
+          last: inputLast,
+          company: inputCompany,
+          email: inputEmail,
+          password: inputPassword,
+        
+        })
+  };
+
+  const [inputFirst, setInputFirst] = useState("");
+  const [inputLast, setInputLast] = useState("");
+  const [inputCompany, setInputCompany] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  return( 
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="signUp-container">
+            <label>Sign Up</label>
+            
+            <div className="signUp-formItems">
+              <label className="signUp-label">First name</label>
+              <input
+                type="text"
+                value={inputFirst}
+                onChange={(e) => setInputFirst(e.target.value)}
+                placeholder="Enter data"
+              />
+            </div>
+            <div className="signUp-formItems">
+              <label className="signUp-label">Last name</label>
+              <input
+                type="text"
+                value={inputLast}
+                onChange={(e) => setInputLast(e.target.value)}
+                placeholder="Enter data"
+              />
+            </div>
+            <div className="signUp-formItems">
+              <label className="signUp-label">Company</label>
+              <input
+                type="text"
+                value={inputCompany}
+                onChange={(e) => setInputCompany(e.target.value)}
+                placeholder="Enter data"
+              />
+            </div>
+            <div className="signUp-formItems">
+              <label className="signUp-label">Email</label>
+              <input
+                type="text"
+                value={inputEmail}
+                onChange={(e) => setInputEmail(e.target.value)}
+                placeholder="Enter data"
+              />
+            </div>
+            <div className="signUp-formItems">
+              <label className="signUp-label">New Password</label>
+              <input
+                type="text"
+                value={inputPassword}
+                onChange={(e) => setInputPassword(e.target.value)}
+                placeholder="Enter data"
+              />
+            </div>
+            <button className="my-button" type="submit">Send Data</button>
+        </div>
+      </form>
+        
+    </div>
+  )
 }
--- server/index.py
+++ server/index.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+app = Flask(__name__)
+CORS(app) # Enable CORS for all routes
 load_dotenv()
 
 mongodb_username = os.getenv("MONGODB_USERNAME")
@@ -19,14 +22,28 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
-
-app = Flask(__name__)
-
+ 
+db = client.userProfile
+profile = db.profile
 
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route('/api/data', methods =['POST'])
+def set_data():
+
+    data = request.get_json()
+        # Process the received data (e.g., save to a database)
+    print(f"Received data: {data}")
+    #if request.method == 'POST':
+    #    content = request.form['content']
+    #    degree = request.form['degree']
+     #   profile.insert_one({'content': content, 'degree': degree})
+    return jsonify({"message": "Data received successfully!", "received_data": data}), 200
+
+  
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
